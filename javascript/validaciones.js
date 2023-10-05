25d87badbe83ea0cf6e6dcbd31582440cabcd3aa
jdiegoweb77 <jdiegozg77@gmail.com>
2023-10-05 16:15:10 -0500

    nuevas funciones

---
+++ javascript/validaciones.js
@@ -0,0 +1,27 @@
+const email = document.getElementById("email");
+const pass = document.getElementById("password");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warning");
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warning = "";
+    let entrar = false;
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+   
+    if(regexEmail.test(email.value)){
+        warning +=  `Invalid email <br>`;
+        entrar = true;
+
+    }
+
+    if(pass.value.lenght < 8){
+        warning +=  `Invalid password <br>`;
+        entrar = true;
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warning;
+    }
+    
+})
